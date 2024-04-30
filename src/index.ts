3809ad786415c3c58dc1797121819757fe7dd8db
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-04-30 13:26:17 +0200

    Add connectToDatabase

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-import { startServer } from "./server/app";
+import "dotenv/config";
+import createDebug from "debug";
+import chalk from "chalk";
+import { startServer } from "./server/app.js";
+import connectToDatabase from "./database/index.js";
+import "./server/index.js";
 
+const debug = createDebug("veambe:index");
 const port = process.env.PORT ?? 5000;
+
+if (!process.env.MONGODB_URL) {
+  debug(chalk.redBright("Missing MONGODB connection string"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
 
 startServer(+port);
